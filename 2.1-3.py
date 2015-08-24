d06d28c680b2eeff72cdf4de80a86be21bc9abdd
wdxfairy <sjtu.wdxfairy@gmail.com>
2015-08-24 21:37:24 +0800

    2.1-3

--- 2.1-3.py
+++ 2.1-3.py
@@ -1,3 +1,11 @@
 __author__ = 'wdxfairy'
 
-for
+def findnum(v,s):
+    L = len(s)
+    for i in range(L):
+        if s[i] == v:
+            return i
+    return None
+
+a = [1,3,54,7,3,2,5,2]
+print(findnum(54,a))
