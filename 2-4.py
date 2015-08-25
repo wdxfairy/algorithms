22db4663a26c9291e1cc910d50219241e0a05f2c
wdxfairy <sjtu.wdxfairy@gmail.com>
2015-08-25 21:45:37 +0800

    2.3-5

---
+++ 2-4.py
@@ -0,0 +1,44 @@
+__author__ = 'wdxfairy'
+
+
+def inversion(s):
+    L = len(s)
+    num = 0
+    if L <= 1:
+        return 0
+    else:
+        mid = L/2
+        num += inversion(s[:mid])
+        num += inversion(s[mid:L])
+        num += merge(s,mid,L)
+    return num
+
+
+def merge(s, p, Length):
+    left = s[:p]
+    right= s[p:Length]
+    c = []
+    numleft = p
+    num = 0
+    while left and right:
+        if left[0] < right[0]:
+            numleft -= 1
+            c.append(left[0])
+            left.pop(0)
+        else:
+            c.append(right[0])
+            num += numleft
+            right.pop(0)
+    if left:
+        c += left
+    if right:
+        c += right
+    s[:] = c[:]
+    return num
+
+a = [7, 6, 5, 4, 3, 2, 1]
+print(inversion(a))
+
+
+
+
